8b42830b638447d6f87038bf4f92d151c579076a
lotanaharoni <50168804+lotanaharoni@users.noreply.github.com>
2021-02-23 11:53:02 +0200

    Update q1.sh

--- q1.sh
+++ q1.sh
@@ -1,14 +1,10 @@
-#!/usr/bin/env bash
-echo "hello world!"
-Var=1
-array=(1 2 3 4 5 6)
-for i in "${array[@]}"; do
-    echo "$i"
-done
 read Name
 cd $Name
-# If the path is not valid
 Contents=$(ls | grep "\.log")
 for i in "${Contents[@]}"; do
-    echo "$i"
+    printf "$i" > temp.txt
+done
+
+for i in "${Contents[@]}"; do
+    grep -c ".*ERROR.*" $i
 done
