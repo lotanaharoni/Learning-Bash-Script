97bd7ebed19262687527d18f904df1744f8fcefa
lotanaharoni <50168804+lotanaharoni@users.noreply.github.com>
2021-02-28 09:10:09 +0200

    Update q1.sh

--- q1.sh
+++ q1.sh
@@ -3,14 +3,13 @@ directory=$@
 # Check if the directory is valid
 if [ -d "$directory" ]; then
     cd $directory
-    # Task 1: List all .log files under a path that will be given as parameter to the job and write them to a temp file
     content=$(ls -a | grep "\.log$")
     # Check if the folder contains any log files
     if [ ! -z "${content}" ]; then
     # Write the log files to a file name 'temp.txt'
         printf "${content}" > temp.txt
 
-        # Task 2: Go over each log file listed in the temp file from #1 and search how many lines contain
+        # Go over each log file listed in the temp file and search how many lines contain
         # the word ERROR and print them to the screen
         grep -c ".*ERROR.*" $content
     else
